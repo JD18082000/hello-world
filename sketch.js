f51a0627a10a5f2751cafaa006d8f8d6033b9eda
JD18082000 <pdo8@uic.edu>
2022-01-26 10:25:32 -0600

    commit

--- sketch.js
+++ sketch.js
@@ -10,30 +10,34 @@ let speedY =5;
 //This is a list of colors
 cLr1= [0,0,0];
 cLr2= [200,20,20];
-cLr3= [0,0,225];
+cLr3= [0,0,255];
+
+clrLIST = [clr1,clr2,clr3];
 
 
 
 function draw() {
-    background (clrList[2]);
-
+    conseole. log(clrlist);
+    console.log(clrlist[1]);
+    //background (clrList[counter][0],clrlist[counter[1]],clrlist[counter[2]]);
+    background(clrlist[counter]);
 
     fill(200,30,30);
     circle (x,y,100);
 
     x= x + speedX;
     y= y + speedY;
-   
+    counter = counter*-1;
 
-    x= x + Speed;
-    if (x >= windowWidth) || x=0)
-    speed = speed(-1)*speed
-
-
-    if (y >= windowWidth) || y=0)
-        speed = speed(-1)*speed;
+    if (counter= clrlist.length-1 || counter <=0){
+        direction = direction*-1
+    }
     
-}
-
+    if(x >= windowwidth || x<=0){
+        speedX= (-1)*speedX;
+    if(y >= windowHeight || y<=0){
+        speedY = (-1)*speedY;
+    }
+    }
 
 }
