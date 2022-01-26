ca70b1a8b399dc39dd8d3848ed2c2b7bd9c03cd1
JD18082000 <pdo8@uic.edu>
2022-01-26 10:10:32 -0600

    commit

--- sketch.js
+++ sketch.js
@@ -2,55 +2,38 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
-newList= [200,255,0,10,104,39,200,48,193];
-x= 0
-Speed = 5
-y = 3
+let x=120;
+let y=100;
+let speedX =5;
+let speedY =5;
 
-
-
-console.log(newList);
-
-firstNum = newList[x];
-console.log(firstNum);
-
-/*
-z= x + y
-console. log(z);
-
-*/
-
-
+//This is a list of colors
+cLr1= [0,0,0];
+cLr2= [200,20,20];
+cLr3= [0,0,225];
 
 
 
 function draw() {
-    background (255,0, 0,(x*0.01));
-
-    //This is the circle on the left
-    strokeweight(4);
-    storke(0,0,255);
-    fill(0,110,255)
-    circle(200,200,10);
-
-    listNum = newList[x];
-
-    //circle on the
-    strokeweight(2);
-    stroke(225,0,255);
-    fill(110,255,0);
-    circle(x,100,20);
-    
-    nofill();
-    strokeweight(1);
-    stroke(0,110,255);
-    circle(300,300,20);
+    background (clrList[2]);
 
 
+    fill(200,30,30);
+    circle (x,y,100);
+
+    x= x + speedX;
+    y= y + speedY;
+   
 
     x= x + Speed;
-    if (x > windowWidth) {
-        speed = speed*-1 
-    }
-    //console.log(x);
+    if (x >= windowWidth) || x=0)
+    speed = speed(-1)*speed
+
+
+    if (y >= windowWidth) || y=0)
+        speed = speed(-1)*speed;
+    
+}
+
+
 }
